bfec9bd780c07012c7fb6a8414857af9f435379d
Sravanthi Patmase <sravanthi.patmase@htcindia.local>
2021-07-30 15:10:46 +0530

    varibale port added

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.get( '/', ( req, res ) => {
 } );
 
 // io.of( '/stream' ).on( 'connection', stream );
-
-server.listen(process.env.port || 3000 );
+var port  = process.env.port || 3000;
+server.listen(port);
 console.log("55555");
 
 io.on("connection", (socket) => {
